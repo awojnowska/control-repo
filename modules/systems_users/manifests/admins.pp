e88fe9f835172448726e479bf8f1e41e05221ce4
root <root@puppet-fundamentals-server-00.local>
2018-05-10 12:48:01 +0000

    add system modules and remove git ignore

---
+++ modules/systems_users/manifests/admins.pp
@@ -0,0 +1,14 @@
+class system_users::admins {
+	
+	package { 'csh':
+		ensure => 'latest',
+		}
+	group { 'staff':
+		ensure => 'present',
+		}
+	user { 'admin':
+		group => 'staff',
+		shell => '/bin/csh/',
+		require => Package['csh']
+		}
+}
