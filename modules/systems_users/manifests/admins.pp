68ad654edec7e3b00469f5f465d9cb64936a1c09
root <root@puppet-fundamentals-server-00.local>
2018-05-10 14:40:53 +0000

    Windows-fied admins.pp

--- modules/systems_users/manifests/admins.pp
+++ modules/systems_users/manifests/admins.pp
@@ -1,14 +1,20 @@
 class system_users::admins {
-	
-	package { 'csh':
-		ensure => 'latest',
-		}
-	group { 'staff':
-		ensure => 'present',
-		}
-	user { 'admin':
-		group => 'staff',
-		shell => '/bin/csh/',
-		require => Package['csh']
-		}
+
+  group {'staff':
+    ensure => present,
+}
+if $facts['kernel'] == 'windows'{
+
+user { 'admin':
+group => 'staff',}
+else {
+package {'csh':
+ensure =>latest,}
+user { 'admin':
+group   => 'staff',
+shell   => '/bin/csh',
+require => Package['csh']
+}
+}
+}
 }
