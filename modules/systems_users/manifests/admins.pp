9cce616abfe30725b674f6f84e0182344ef9489f
root <root@puppet-fundamentals-server-00.local>
2018-05-10 15:06:09 +0000

    Fixed formatting

--- modules/systems_users/manifests/admins.pp
+++ modules/systems_users/manifests/admins.pp
@@ -2,19 +2,21 @@ class systems_users::admins {
 
   group {'staff':
     ensure => present,
-}
-if $facts['kernel'] == 'windows'{
+  }
 
-user { 'admin':
-groups => 'staff',}
-else {
-package {'csh':
-ensure =>latest,}
-user { 'admin':
-groups  => 'staff',
-shell   => '/bin/csh',
-require => Package['csh']
-}
-}
-}
+  if $facts['kernel'] == 'windows'{
+
+    user { 'admin':
+      groups => 'staff',
+    }
+  } else {
+    package {'csh':
+      ensure =>latest,
+    }
+    user { 'admin':
+      groups  => 'staff',
+      shell   => '/bin/csh',
+      require => Package['csh']
+    }
+  }
 }
